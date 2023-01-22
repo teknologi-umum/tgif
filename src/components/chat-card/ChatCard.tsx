7323a601f4e869a0e7bd233efddd3a02cac1d082
elianiva <dicha.arkana03@gmail.com>
2023-01-22 18:19:28 +0700

    chore: use boring avatars instead

--- src/components/chat-card/ChatCard.tsx
+++ src/components/chat-card/ChatCard.tsx
@@ -4,7 +4,6 @@ import "./ChatCard.scss";
 type ChatCardProps = Metadata;
 
 export function ChatCard(props: ChatCardProps) {
-	const avatarSeed = props.title.replaceAll(" ", "").slice(0, 8);
 	return (
 		<a
 			href={`/chat/${props.id}`}
@@ -19,7 +18,9 @@ export function ChatCard(props: ChatCardProps) {
 				<div class="profile-image">
 					<img
 						alt={props.title}
-						src={`https://api.dicebear.com/5.x/adventurer/svg?seed=${avatarSeed}&backgroundColor=b6e3f4,c0aede,d1d4f9&scale=80`}
+						src={`https://source.boringavatars.com/beam/120/${encodeURIComponent(
+							props.title
+						)}?colors=fca2e1,93b5ff,6be4dc,f9e3a9,4a6cb6`}
 					/>
 				</div>
 				<span class="name">{props.title}</span>
