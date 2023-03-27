a958935c3918e82923b7a013e04a21a319afa833
rijalBinHusen <jeryzal333@gmail.com>
2023-03-27 07:55:06 +0700

    styling with class name first, like dicha said 🙏

--- src/components/chat-bubble/ChatBubble.tsx
+++ src/components/chat-bubble/ChatBubble.tsx
@@ -48,7 +48,6 @@ export function ChatBubble(props: ChatBubbleProps) {
 					<img
 						class={resolveMediaClassname()}
 						src={props.hasMedia ? `/chats/${props.slug}/${props.file}` : ""}
-						style={{ 'width': '100%', 'padding': '0.8rem'}}
 					/>
 					<Show when={props.text.length}>
 						<div
