98d2d5fb73cf9fee372f775da236401b6047d65b
Muhammad Yusuf <yusuf@iamyuu.dev>
2023-09-02 21:29:31 +0700

    revert: sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,9 @@
-import { createSignal, For, onCleanup, onMount } from "solid-js";
+import { createEffect, createSignal, For, onCleanup, type JSX, Switch, Match, onMount } from "solid-js";
 import autoAnimate from "@formkit/auto-animate";
-// import { debounce } from "@solid-primitives/scheduled";
+import { debounce } from "@solid-primitives/scheduled";
 import type { Metadata } from "~/service/Metadata";
-// import SearchIcon from "~icons/fluent/search-24-filled";
-// import ArrowIcon from "~icons/fluent/arrow-left-12-regular";
+import SearchIcon from "~icons/fluent/search-24-filled";
+import ArrowIcon from "~icons/fluent/arrow-left-12-regular";
 import { ChatCard } from "../chat-card";
 import "./Sidebar.scss";
 import { storageKeys } from "~/utils/constants";
@@ -15,8 +15,10 @@ type SidebarProps = {
 
 export function Sidebar(props: SidebarProps) {
 	let chatsContentEl!: HTMLDivElement;
-	const [keyword] = createSignal("");
-	const pathname = () => (typeof window === "undefined" ? props.pathname : window.location.pathname);
+	const [keyword, setKeyword] = createSignal("");
+	const [pathname, setPathname] = createSignal(
+		typeof window === "undefined" ? props.pathname : window.location.pathname
+	);
 	const isPathEmpty = () => pathname() === "/";
 
 	function matchWithKeyword(talk: Metadata) {
@@ -38,15 +40,23 @@ export function Sidebar(props: SidebarProps) {
 		return hasMatch;
 	}
 
-	// const updateKeyword: JSX.EventHandler<HTMLInputElement, InputEvent> = debounce(
-	// 	(e) => setKeyword((e.target as HTMLInputElement).value),
-	// 	250
-	// );
+	const updateKeyword: JSX.EventHandler<HTMLInputElement, InputEvent> = debounce(
+		(e) => setKeyword((e.target as HTMLInputElement).value),
+		250
+	);
 
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	function animate(el: HTMLDivElement) {
 		autoAnimate(el, { duration: 200 });
 	}
+
+	createEffect(() => {
+		const updatePathname = (navigateEvent: NavigateEvent) => {
+			setPathname(new URL(navigateEvent.destination.url).pathname);
+		};
+		window.navigation.addEventListener("navigate", updatePathname);
+		onCleanup(() => window.navigation.removeEventListener("navigate", updatePathname));
+	});
 
 	function storeCurrentScrollPositionToSessionStorage() {
 		window.sessionStorage.setItem(storageKeys.sidebarScrollY, chatsContentEl.scrollTop.toString());
@@ -64,22 +74,55 @@ export function Sidebar(props: SidebarProps) {
 	});
 
 	return (
-		<div ref={chatsContentEl} class="chats-content" use:animate>
-			<For each={props.talks.filter(matchWithKeyword)}>
-				{(chat) => (
-					<ChatCard
-						{...chat}
-						isActive={isPathEmpty() ? true : `/chat/${chat.slug}` === pathname()}
-						onClick={() => {
-							document.title = `${chat.title} | TGIF`;
-							window.sessionStorage.setItem(
-								storageKeys.sidebarScrollY,
-								chatsContentEl.scrollTop.toString()
-							);
-						}}
-					/>
-				)}
-			</For>
-		</div>
+		<aside class="sidebar">
+			<div class="search-container">
+				<div class="search-shadow" />
+				<input
+					class={`search-input ${isPathEmpty() ? "" : "search-input-active"}`}
+					type="text"
+					onInput={updateKeyword}
+					value={keyword()}
+					placeholder="Search something... (add # for tags)"
+				/>
+				<div
+					class={`search-icon ${isPathEmpty() ? "" : "search-icon-active"}`}
+					onClick={() => {
+						if (!isPathEmpty()) {
+							document.title = "Home | TGIF";
+							window.location.replace("/");
+							window.sessionStorage.clear();
+						}
+					}}
+				>
+					<Switch>
+						<Match when={!isPathEmpty()}>
+							<ArrowIcon />
+						</Match>
+						<Match when={isPathEmpty()}>
+							<SearchIcon />
+						</Match>
+					</Switch>
+				</div>
+			</div>
+			<div class="chats-container">
+				<div ref={chatsContentEl} class="chats-content" use:animate>
+					<For each={props.talks.filter(matchWithKeyword)}>
+						{(chat) => (
+							<ChatCard
+								{...chat}
+								isActive={isPathEmpty() ? true : `/chat/${chat.slug}` === pathname()}
+								onClick={() => {
+									document.title = `${chat.title} | TGIF`;
+									window.sessionStorage.setItem(
+										storageKeys.sidebarScrollY,
+										chatsContentEl.scrollTop.toString()
+									);
+								}}
+							/>
+						)}
+					</For>
+				</div>
+			</div>
+		</aside>
 	);
 }
